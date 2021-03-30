2718a18d2cbf075111333bf6bcae5b6d45d320de
sourav <saurav2fly@gmail.com>
2021-03-30 17:25:48 +0530

    added migration script

--- migrations/2_deploy_token.js
+++ migrations/2_deploy_token.js
@@ -1,8 +1,18 @@
 const MyToken = artifacts.require('MyToken');
+const Staking = artifacts.require('Staking');
 
-module.exports = function (deployer) {
+module.exports = async function (deployer) {
+  // deploying token
   const _name = 'MyToken';
   const _symbol = 'MTK';
   const totalsupply = 1000000;
-  deployer.deploy(MyToken, _name, _symbol, totalsupply);
+  await deployer.deploy(MyToken, _name, _symbol, totalsupply);
+  const myToken = await MyToken.deployed();
+
+  // deploying staking smartcontract
+  await deployer.deploy(Staking, myToken.address);
+  const staking = await Staking.deployed();
+
+  // transfering 50% of the token to staking smart contract
+  await myToken.transfer(staking.address, '500000');
 };
